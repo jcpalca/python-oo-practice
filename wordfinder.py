a194242d4c679297d86d1a8bd151d776711a2f5b
Christopher Lim <cli3338198@gmail.com>
2022-10-11 17:17:31 -0700

    finished oo

--- wordfinder.py
+++ wordfinder.py
@@ -1,7 +1,7 @@
 from random import choice
 
 class WordFinder:
-    """Word Finder: finds random words from a dictionary.
+    """ Word Finder: finds random words from a dictionary.
 
         >>> wf = WordFinder()
         3 words read
@@ -21,13 +21,70 @@ class WordFinder:
     """
 
     def __init__(self, file_path='words.txt'):
+        """ reads the file
+            reports the number of words read
+            closes the file
+        """
         self.file_path = file_path
         self.file = open(file_path)
-        self.words_list = []
-        for line in self.file:
-            self.words_list.append(line.strip())
+        self.words_list = self.read_lines(self.file)
+
         print(f"{len(self.words_list)} words read")
 
+        self.file.close()
+
+    def __repr__(self):
+        """ represents the word finder instance 
+            includes the file path, first three words, # of words in list
+        """
+        return f"""<{self.__class__.__name__}
+            file_path={self.file_path} 
+            first_three_words={self.words_list[0:3:]}
+            num_words={len(self.words_list)}
+        >"""
 
     def random(self):
+        """ returns a random element from words list
+        """
         return choice(self.words_list)
+
+    def read_lines(self, open_file):
+        """ reads the file and strips "\\n" character
+            returns a list of words from file
+        """
+        return [word.strip() for word in open_file]
+
+class SpecialWordFinder(WordFinder):
+    """ Special word finder that filters out #comments and empty lines
+
+        >>> swf = SpecialWordFinder("foods.txt")
+        4 words read
+
+        >>> swf.random()
+        'apple'
+
+        >>> swf.random()
+        'mango'
+
+        >>> swf.random()
+        'kale'
+
+        >>> swf.random()
+        'parsnips'
+
+    """
+
+    # def __init__(self, file_path):
+    #     super().__init__(file_path)
+        
+    def read_lines(self, open_file):
+        """ filters out empty lines and comments from the parent's list of words
+            return a new filtered list
+        """
+        return [word for word in super().read_lines(open_file) 
+                if not word.startswith("#") and not word == ""]
+    
+
+    
+
+    

f42a624d3aa3950e1070d07ebbb175167121d2cb
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-11 17:24:30 -0700

    Docstring updates and fixes

--- wordfinder.py
+++ wordfinder.py
@@ -34,11 +34,11 @@ class WordFinder:
         self.file.close()
 
     def __repr__(self):
-        """ represents the word finder instance 
+        """ represents the word finder instance
             includes the file path, first three words, # of words in list
         """
         return f"""<{self.__class__.__name__}
-            file_path={self.file_path} 
+            file_path={self.file_path}
             first_three_words={self.words_list[0:3:]}
             num_words={len(self.words_list)}
         >"""
@@ -49,7 +49,7 @@ class WordFinder:
         return choice(self.words_list)
 
     def read_lines(self, open_file):
-        """ reads the file and strips "\\n" character
+        """ reads the file and strips "\\n" and "\\r" characters
             returns a list of words from file
         """
         return [word.strip() for word in open_file]
@@ -74,17 +74,9 @@ class SpecialWordFinder(WordFinder):
 
     """
 
-    # def __init__(self, file_path):
-    #     super().__init__(file_path)
-        
     def read_lines(self, open_file):
-        """ filters out empty lines and comments from the parent's list of words
+        """ filters out empty lines and #comments from parent's list of words
             return a new filtered list
         """
-        return [word for word in super().read_lines(open_file) 
+        return [word for word in super().read_lines(open_file)
                 if not word.startswith("#") and not word == ""]
-    
-
-    
-
-    
